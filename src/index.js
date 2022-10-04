c0aabdfc362357d75d1ec99f7dc108c1a82011d2
Cole <cole@colevscode.io>
2022-10-03 17:14:24 -0700

    adding status message in case no formspree.json exists

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-fs = require('fs')
+fs = require("fs");
 
 /* eslint-disable no-unused-vars */
 module.exports = {
-  async onPostBuild({ utils: { build, run } }) {
-    if (fs.existsSync('formspree.json')) {
+  async onPostBuild({ utils: { build, run, status } }) {
+    if (fs.existsSync("formspree.json")) {
       try {
-        await run('formspree', ['deploy'])
+        await run("formspree", ["deploy"]);
       } catch (error) {
         // Report a user error
-        build.failBuild('Formspree deploy command failed. See errors above.')
+        build.failBuild("Formspree deploy command failed. See errors above.");
       }
+    } else {
+      status.show({
+        title: "Formspree deploy skipped.",
+        summary: "No formspree.json file found.",
+      });
     }
   },
-}
+};
